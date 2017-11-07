5f4f4694c4e41aa3d0453821fb79b638f82cee91
Nalipp <nateplipp@gmail.com>
2017-11-07 17:38:45 -0600

    arrays

---
+++ hacker_rank/algorithms/algorithms/easy/designer_pdf_viewer.js
@@ -0,0 +1,17 @@
+// var h = [ 1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 ];
+// var word = 'abc';  // 9
+
+var h = [1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7];
+var word = 'zaba';  // 28
+
+let alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+let maxHeight = 0;
+for (let i = 0; i < word.length; i++) {
+  maxHeight = Math.max(maxHeight, h[alpha.indexOf(word[i])]);
+}
+console.log(word.length * maxHeight);
+
+
+
+

0e15412324077290de41bb42c9adcd094bd24700
Nalipp <nateplipp@gmail.com>
2017-11-28 09:49:59 -0600

    cleanup

--- cs_meetup_prep/cs_practice_problems/recursion/addSmallerDigits.js
+++
@@ -1,6 +0,0 @@
-let addSmallerDigits = (num) => {
-  if (num === 1) return 1;
-  return num + addSmallerDigits(num - 1);
-}
-
-console.log(addSmallerDigits(8)); 
--- cs_meetup_prep/cs_practice_problems/recursion/printRange.js
+++
@@ -1,7 +0,0 @@
-let printRange = (min, max) => {
-  if (min === max) return console.log(max);
-  console.log(min);
-  return printRange(min + 1, max);
-}
-
-printRange(3, 7);

c0ea2f21a39d5490909008b98b99674a26bb44a5
Nalipp <nateplipp@gmail.com>
2017-12-21 12:30:17 -0600

    superbalanced

--- interview_cake/all/8_superbalanced.js
+++ interview_cake/all/8_superbalanced.js
@@ -1,28 +1,68 @@
-function BinaryTreeNode(value) {
-  this.value = value;
-  this.left  = null;
+function Node(val) {
+  this.val = val;
+  this.left = null;
   this.right = null;
 }
 
-BinaryTreeNode.prototype.insertLeft = function(value) {
-  this.left = new BinaryTreeNode(value);
-  return this.left;
-};
-
-BinaryTreeNode.prototype.insertRight = function(value) {
-  this.right = new BinaryTreeNode(value);
-  return this.right;
-};
-
-let root = new BinaryTreeNode(15);
-root.insertLeft(8);
-root.insertRight(20);
-
-root.left.insertLeft(5);
-root.left.insertRight(12);
-root.left.right.insertRight(10);
-
-BinaryTreeNode.prototype.print = function() {
+Node.prototype.add = function(val) {
+  let newNode = new Node(val);
+  let current = this;
+  while (current) {
+    if (val < current.val) {
+      if (!current.left) return current.left = newNode;
+      current = current.left;
+    }
+    else if (val > current.val) {
+      if (!current.right) return current.right = newNode;
+      current = current.right;
+    }
+    else throw new Error('New node must have a unique value')
+  }
 }
 
-console.log(root.print());
+Node.prototype.findMaxDepth = function() {
+  let maxDepth = Number.MIN_VALUE;
+  innerFindMax(this, 0);
+  function innerFindMax(current, depth) {
+    maxDepth = Math.max(maxDepth, depth);
+    depth++;
+    if (current.left) innerFindMax(current.left, depth);
+    if (current.right) innerFindMax(current.right, depth);
+  }
+  return maxDepth;
+}
+
+Node.prototype.findMinDepth = function() {
+  let minDepth = Number.MAX_VALUE;
+  innerFindMin(this, 0);
+  function innerFindMin(current, depth) {
+    if (!current.left && !current.right) minDepth = Math.min(minDepth, depth);
+    depth++;
+    if (current.left) innerFindMin(current.left, depth);
+    if (current.right) innerFindMin(current.right, depth);
+  }
+  return minDepth;
+}
+
+Node.prototype.isSuperBalanced = function() {
+  return (this.findMaxDepth() - this.findMinDepth()) < 2
+}
+
+let tree = new Node(15);
+tree.add(22);
+tree.add(23);
+tree.add(24);
+tree.add(10);
+tree.add(6);
+tree.add(7);
+tree.add(8);
+tree.add(9);
+tree.add(14);
+tree.add(13);
+tree.add(12);
+tree.add(11);
+
+tree.findMaxDepth();
+tree.findMinDepth();
+console.log(tree.isSuperBalanced());
+

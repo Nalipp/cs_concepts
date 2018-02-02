e9191ce3c581e762ad639e87edb664d13cc5d97f
Nalipp <nateplipp@gmail.com>
2018-02-02 15:39:58 -0600

    reverse int

--- udemy/AlgoCasts/exercises/maxchar/index.js
+++ udemy/AlgoCasts/exercises/maxchar/index.js
@@ -5,6 +5,19 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let maxChar;
+  let maxCount = 0;
+  let hash = {};
+  for (let i = 0; i < str.length; i++) {
+    let c = str[i];
+    if (hash[c]) hash[c] += 1;
+    else hash[c] = 1;
+
+    maxCount = Math.max(maxCount, hash[c]);
+    maxChar = (maxCount > hash[c]) ? maxChar : c; 
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
--- udemy/AlgoCasts/exercises/reverseint/index.js
+++ udemy/AlgoCasts/exercises/reverseint/index.js
@@ -8,6 +8,21 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(num) {
+  let mod = 10;
+  let reversed = 0;
+  let absNum = Math.abs(num);                                
+  while (true) {
+    let currentNum = Math.floor((absNum % mod) / (mod / 10));
+    reversed = (reversed * 10) + currentNum;
+    if (absNum % mod === absNum) break;
+    mod *= 10;
+  }
+  return reversed * Math.sign(num);
+}
 
 module.exports = reverseInt;
+
+// let reversed = Number((Math.abs(num) + '').split('').reverse().join(''));
+// return reversed * Math.sign(num);
+

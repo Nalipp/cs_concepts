eb906944e87ab40f9906636aa710662b2db3699b
Nalipp <nateplipp@gmail.com>
2018-02-02 12:11:21 -0600

    palindrome

--- udemy/AlgoCasts/exercises/palindrome/index.js
+++ udemy/AlgoCasts/exercises/palindrome/index.js
@@ -7,6 +7,26 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  for ( let i = 0, j = str.length - 1; 
+        i < Math.ceil(str.length / 2); 
+        i++, j--
+      ) {
+        if (str[i] !== str[j]) return false;
+      }
+  return true
+}
 
 module.exports = palindrome;
+
+// function palindrome(str) {
+//   return str.split('').reduce((r, c) => c + r, '') === str;
+// }
+
+// function palindrome(str) {
+//   let result = '';
+//   for (let char of str) {
+//     result = char + result;
+//   }
+//   return result === str;
+// }

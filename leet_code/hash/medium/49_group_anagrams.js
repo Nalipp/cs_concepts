20bbf5ba3f2d826172f6aa77474d0964deab9457
Nalipp <nateplipp@gmail.com>
2018-04-30 09:20:48 -0700

    meetup hashes

---
+++ leet_code/hash/medium/49_group_anagrams.js
@@ -0,0 +1,35 @@
+// group anagrams
+//
+// Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+// Output:
+// [
+//   ["ate","eat","tea"],
+//   ["nat","tan"],
+//   ["bat"]
+// ]
+
+var groupAnagrams = function(strs) {
+  var obj = {};
+  var results = [];
+
+  for (var word of strs) {
+    var sortedWord = word.split('').sort((a, b) => a > b).join('');
+
+    if (obj[sortedWord]) {
+
+      obj[sortedWord].push(word);
+    } else {
+
+      obj[sortedWord] = [word];      
+    }
+  }
+
+  for (var key in obj) {
+    results.push(obj[key]);
+  }
+
+  return results;
+};
+
+var strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+console.log(groupAnagrams(strs))

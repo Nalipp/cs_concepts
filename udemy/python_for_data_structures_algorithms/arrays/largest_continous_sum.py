b3464226e494ebda6f817d3278a24b8de334bd83
Nalipp <nateplipp@gmail.com>
2018-06-12 08:56:00 -0700

    python largest continuous sum

--- udemy/python_for_data_structures_algorithms/arrays/largest_continous_sum.py
+++ udemy/python_for_data_structures_algorithms/arrays/largest_continous_sum.py
@@ -1,17 +1,15 @@
 def large_cont_sum1(arr):
-    max_sum = 0
+    if len(arr) == 0:
+        return 0
 
-    for i in range(len(arr) - 1):
-        first = arr[i]
-        cur_sum = first
-        
-        for j in range(i + 1, len(arr)):
-            sec = arr[j]
+    max_sum = cur_sum = arr[0]
 
-            if sec > max_sum:
-                max_sum = sec
+    for num in arr[1:]:
 
-            cur_sum += sec
+        if (cur_sum + num) < 0:
+            cur_sum = 0
+        else:
+            cur_sum += num
 
             if cur_sum > max_sum:
                 max_sum = cur_sum
@@ -19,26 +17,30 @@ def large_cont_sum1(arr):
     return max_sum
 
 
-
 def large_cont_sum2(arr):
-    if len(arr) == 2:
-        return max(arr[0], arr[1])
+    if len(arr) == 0:
+        return 0
 
-    def inner(arr):
-        if len(arr) < 2:
-            return 0
+    max_sum = cur_sum = arr[0]
 
-        return max(arr[0] + large_cont_sum2(arr[1:]), large_cont_sum2(arr[2:]))
+    for num in arr[1:]:
+        cur_sum = max(cur_sum + num, num)
+        max_sum = max(cur_sum, max_sum)
 
-    return inner(arr)
+    return max_sum
+
+
 
 
 
 # print(large_cont_sum1([1,2,-1,3,4,-1])) # => 9
-# print(large_cont_sum1([1,2,-1,3,4,10,10,-10,-1])) # => 29
+# print(large_cont_sum1([1,2,-1,3,4,10,10,-10,-1,-10,-10,-10,10])) # => 29
 # print(large_cont_sum1([-1,1])) # => 1
+# print(large_cont_sum1([-1,-2,-1,-3,-4,-1])) # => 0
 
 print(large_cont_sum2([1,2,-1,3,4,-1])) # => 9
-print(large_cont_sum2([1,2,-1,3,4,10,10,-10,-1])) # => 29
+print(large_cont_sum2([1,2,-1,3,4,10,10,-10,-1,-10,-10,-10,10])) # => 29
 print(large_cont_sum2([-1,1])) # => 1
+print(large_cont_sum2([-1,-2,-1,-3,-4,-1])) # => -1
+
 
